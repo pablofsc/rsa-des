ab0342bb399755bf13f12c7a636111a080468f34
pablofsc <pablofsc@outlook.com>
2022-06-26 15:41:03 -0300

    Melhora suporte a telas pequenas

--- script_rsa.js
+++ script_rsa.js
@@ -17,21 +17,21 @@ const atualizar = () => {
 
     if (p === q)
         $('#avisos').html(`
-        <div class="text-bg-danger rounded-3 p-2">
-            <small>
-                <b>Os primos selecionados são iguais</b><br>
-                O método RSA requer dois primos diferentes.
-            </small>
-        </div>
+            <div class="text-bg-danger rounded-2 p-2">
+                <small>
+                    <b>Os primos selecionados são iguais</b><br>
+                    O método RSA requer dois primos diferentes.
+                </small>
+            </div>
         `);
     else if (params.n < 400)
         $('#avisos').html(`
-        <div class="text-bg-warning rounded-3 p-2">
-            <small>
-                <b>Os primos selecionados são muito pequenos</b><br>
-                A descriptografia falha se a mensagem contiver um caractere cujo código seja maior que n. (tipicamente letras com acentos)
-            </small>
-        </div>
+            <div class="text-bg-warning rounded-2 p-2">
+                <small>
+                    <b>Os primos selecionados são muito pequenos</b><br>
+                    A descriptografia falha se a mensagem contiver um caractere cujo código seja maior que n. (tipicamente letras com acentos)
+                </small>
+            </div>
         `);
     else $('#avisos').html('');
 
@@ -40,11 +40,11 @@ const atualizar = () => {
 
     const mensagem = paraCodigos($('#entrada-cifrar').val());
 
-    const cifrado = cifrarRSA(params, mensagem);
+    const cifrado = cifrarRSA(mensagem, params.e, params.n);
     $('#saida-cifrar').html(paraCaracteres(cifrado));
     $('#entrada-decifrar').html(paraCaracteres(cifrado));
 
-    const decifrado = decifrarRSA(params, cifrado);
+    const decifrado = decifrarRSA(cifrado, params.d, params.n);
     $('#saida-decifrar').html(paraCaracteres(decifrado));
 };
 
@@ -69,23 +69,18 @@ const inserirPrimos = () => {
     $(`#q > *[value='23']`).attr('selected', '');
 };
 
-const n = (p, q) => {
-    return p * q;
-};
-
-const phi = (p, q) => {
-    return (p - 1) * (q - 1);
-};
-
-const publicKey = (phi) => {
-    for (let e = 2; e < phi; e++) if (saoCoprimos(phi, e)) return e;
-};
-
-const privateKey = (e, phi) => {
-    for (let d = 0; ; d++) if ((d * e) % phi === 1) return d;
-};
-
 const prepararRSA = (p, q) => {
+    const n = (p, q) => p * q;
+    const phi = (p, q) => (p - 1) * (q - 1);
+
+    const publicKey = (phi) => {
+        for (let e = 2; e < phi; e++) if (saoCoprimos(phi, e)) return e;
+    };
+
+    const privateKey = (e, phi) => {
+        for (let d = 0; ; d++) if ((d * e) % phi === 1) return d;
+    };
+
     const N = n(p, q);
     const PHI = phi(p, q);
     const E = publicKey(PHI);
@@ -101,21 +96,21 @@ const prepararRSA = (p, q) => {
     };
 };
 
-const cifrarRSA = (parametros, mensagem) => {
+const cifrarRSA = (mensagem, e, n) => {
     let cifrado = [];
     for (let codigoCaractere of mensagem) {
-        const c = expMod(codigoCaractere, parametros.e, parametros.n);
-        cifrado.push(c);
+        const letraCifrada = expMod(codigoCaractere, e, n);
+        cifrado.push(letraCifrada);
     }
 
     return cifrado;
 };
 
-const decifrarRSA = (parametros, cifrado) => {
+const decifrarRSA = (cifrado, d, n) => {
     let decifrado = [];
     for (let codigoCaractere of cifrado) {
-        const d = expMod(codigoCaractere, parametros.d, parametros.n);
-        decifrado.push(d);
+        const letraDecifrada = expMod(codigoCaractere, d, n);
+        decifrado.push(letraDecifrada);
     }
 
     return decifrado;
